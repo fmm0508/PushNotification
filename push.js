d4e5b5cbce414a9436ac1307c730cc32c35a59bc
Farahani Mahendo <farahani.mahendo@rwjbh.org>
2022-06-06 15:09:50 -0400

    Forth Comment

--- push.js
+++ push.js
@@ -7,6 +7,14 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:faraghoney@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {};
+// THIS PART WILL BE SAVED IN THE BACKEND.
+let sub = {
+  "endpoint":"https://fcm.googleapis.com/fcm/send/fTP0ZVP_e-w:APA91bE8n1myPKCikax5gVVUo1z82KFJptDjsB6brBjqSJ1lIpqL4ptH4b5a25Ee0pu1J3X3wTBtFNNYGRf2sOLYD3Ic0WLsJl8BhBIoIse5zJudf55uNnlsrBOmDkhBuevl-tNOYK8k",
+  "expirationTime":null,
+  "keys":{
+    "p256dh":"BFnLtxuvl4R5CLqYDG1ofLC5wqY_bYSkOHqtK9sJdMUvykwHvWEFu2jSejyM4SXvFfWWuqB6sapF2JJzGc1Xaj8",
+    "auth":"q1b_Ti0iqIZBLvciHZQueQ"
+  }
+};
 
 push.sendNotification(sub, 'You have a patient to pick up');
